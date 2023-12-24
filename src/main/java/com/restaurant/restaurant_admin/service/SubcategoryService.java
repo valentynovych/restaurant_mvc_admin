a4c5d0164da776a37fbaff16de1b43a88c5b1d2f
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2023-12-24 23:19:21 +0200

    Tests for SubcategoryService.java

--- src/main/java/com/restaurant/restaurant_admin/service/SubcategoryService.java
+++ src/main/java/com/restaurant/restaurant_admin/service/SubcategoryService.java
@@ -1,17 +1,14 @@
 package com.restaurant.restaurant_admin.service;
 
+import com.restaurant.restaurant_admin.entity.MainCategory;
 import com.restaurant.restaurant_admin.entity.Subcategory;
 import com.restaurant.restaurant_admin.mapper.MainCategoryMapper;
 import com.restaurant.restaurant_admin.model.SubcategoryDTO;
-import com.restaurant.restaurant_admin.entity.MainCategory;
-import com.restaurant.restaurant_admin.mapper.SubcategoryMapper;
 import com.restaurant.restaurant_admin.repository.SubcategoryRepo;
 import com.restaurant.restaurant_admin.repository.specification.SearchCriteria;
 import com.restaurant.restaurant_admin.repository.specification.SubcategorySpecification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -21,26 +18,31 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@Qualifier
 @RequiredArgsConstructor
 @Log4j2
 public class SubcategoryService {
     private final SubcategoryRepo subcategoryRepo;
-    //private final MainCategoryMapper mapper;
 
     public List<SubcategoryDTO> getSubcategoriesByParent(Long parentCategory) {
+        log.info("getSubcategoriesByParent() -> start, with parent category id: " + parentCategory);
         MainCategory mainCategory = new MainCategory();
         mainCategory.setId(parentCategory);
-        return MainCategoryMapper.MAPPER.subcategoryListToDto(subcategoryRepo.findAllByParentCategory(mainCategory));
+        List<Subcategory> allByParentCategory = subcategoryRepo.findAllByParentCategory(mainCategory);
+        List<SubcategoryDTO> subcategoryDTOS = MainCategoryMapper.MAPPER.subcategoryListToDto(allByParentCategory);
+        log.info("getSubcategoriesByParent() -> exit, return list size: " + subcategoryDTOS.size());
+
+        return subcategoryDTOS;
     }
 
     public Page<SubcategoryDTO> getSubcategories(int page, int pageSize, String search) {
+        log.info("getSubcategories() -> start, with search: " + search);
         Pageable pageable = PageRequest.of(page, pageSize);
         SubcategorySpecification likeName = new SubcategorySpecification(
                 new SearchCriteria("subcategoryName", ":", search));
         Page<Subcategory> subcategoryPage = subcategoryRepo.findAll(likeName, pageable);
         List<SubcategoryDTO> dtoList = MainCategoryMapper.MAPPER.subcategoryListToDto(subcategoryPage.getContent());
         Page<SubcategoryDTO> subcategoryDTOPage = new PageImpl<>(dtoList, pageable, subcategoryPage.getTotalElements());
+        log.info("getSubcategories() -> exit");
         return subcategoryDTOPage;
     }
 }
---
+++ src/test/java/com/restaurant/restaurant_admin/service/SubcategoryServiceTest.java
@@ -0,0 +1,85 @@
+package com.restaurant.restaurant_admin.service;
+
+import com.restaurant.restaurant_admin.entity.MainCategory;
+import com.restaurant.restaurant_admin.entity.Subcategory;
+import com.restaurant.restaurant_admin.model.SubcategoryDTO;
+import com.restaurant.restaurant_admin.repository.SubcategoryRepo;
+import com.restaurant.restaurant_admin.repository.specification.SubcategorySpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class SubcategoryServiceTest {
+
+    @Mock
+    private SubcategoryRepo subcategoryRepo;
+    @InjectMocks
+    private SubcategoryService subcategoryService;
+    private List<Subcategory> subcategories;
+
+    @BeforeEach
+    void setUp() {
+        subcategories = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Subcategory subcategory = new Subcategory();
+            subcategory.setId((long) i);
+            subcategory.setSubcategoryName("subcategory_" + i);
+            subcategories.add(subcategory);
+        }
+    }
+
+    @Test
+    void getSubcategoriesByParent() {
+        MainCategory mainCategory = new MainCategory();
+        mainCategory.setId(1L);
+
+        when(subcategoryRepo.findAllByParentCategory(any(MainCategory.class))).thenReturn(subcategories);
+
+        List<SubcategoryDTO> subcategoriesByParent = subcategoryService.getSubcategoriesByParent(mainCategory.getId());
+        assertFalse(subcategoriesByParent.isEmpty());
+        for (int i = 0; i < subcategories.size(); i++) {
+            Subcategory subcategory = subcategories.get(i);
+            SubcategoryDTO subcategoryDTO = subcategoriesByParent.get(i);
+            assertEquals(subcategory.getId(), subcategoryDTO.getSubcategoryId());
+            assertEquals(subcategory.getSubcategoryName(), subcategoryDTO.getSubcategoryName());
+        }
+    }
+
+    @Test
+    void getSubcategories() {
+        Pageable pageable = PageRequest.ofSize(10);
+        Page<Subcategory> subcategoryPage = new PageImpl<>(subcategories, pageable, subcategories.size());
+        when(subcategoryRepo.findAll(any(SubcategorySpecification.class), any(Pageable.class)))
+                .thenReturn(subcategoryPage);
+
+        Page<SubcategoryDTO> subcategories1 =
+                subcategoryService.getSubcategories(pageable.getPageNumber(), pageable.getPageSize(), "");
+        List<SubcategoryDTO> content = subcategories1.getContent();
+        assertFalse(content.isEmpty());
+
+        for (int i = 0; i < subcategories.size(); i++) {
+            Subcategory subcategory = subcategories.get(i);
+            SubcategoryDTO subcategoryDTO = content.get(i);
+            assertEquals(subcategory.getId(), subcategoryDTO.getSubcategoryId());
+            assertEquals(subcategory.getSubcategoryName(), subcategoryDTO.getSubcategoryName());
+        }
+
+
+    }
+}
