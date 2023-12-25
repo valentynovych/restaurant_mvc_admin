7b92f65f57809b1d78be7e2199e3cb755a6b6d70
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2023-12-25 03:23:09 +0200

    Tests for PromotionService.java and add logs to this service

--- src/main/java/com/restaurant/restaurant_admin/service/PromotionService.java
+++ src/main/java/com/restaurant/restaurant_admin/service/PromotionService.java
@@ -9,7 +9,6 @@ import com.restaurant.restaurant_admin.repository.PromotionRepo;
 import com.restaurant.restaurant_admin.repository.specification.PromotionSpecification;
 import com.restaurant.restaurant_admin.repository.specification.SearchCriteria;
 import com.restaurant.restaurant_admin.utils.UploadFileUtil;
-import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -18,7 +17,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -38,16 +36,20 @@ public class PromotionService {
     private final UploadFileUtil fileUtil;
 
     public Page<PromotionResponse> getAllPromotions(int page, int pageSize) {
+        log.info(String.format("getAllPromotions() -> start, with page: %s, pageSize: %s", page, pageSize));
         Pageable pageable = PageRequest.of(page, pageSize);
         Page<Promotion> promotionPage = promotionRepo.findAll(pageable);
         List<PromotionResponse> promotionShortList =
                 PromotionMapper.MAPPER.listPromotionToPromotionResponse(promotionPage.getContent());
         Page<PromotionResponse> responsePage =
                 new PageImpl<>(promotionShortList, pageable, promotionPage.getTotalElements());
+        log.info("getAllPromotions() -> exit");
         return responsePage;
     }
 
     public Page<PromotionResponse> getAllPromotionsBySearch(int page, int pageSize, String search) {
+        log.info(String.format("getAllPromotionsBySearch() -> start, with page: %s, pageSize: %s, by search: %s",
+                page, pageSize, search));
         Pageable pageable = PageRequest.of(page, pageSize);
         SearchCriteria likeName = new SearchCriteria("name", ":", search);
         Page<Promotion> promotionPage = promotionRepo.findAll(new PromotionSpecification(likeName), pageable);
@@ -55,52 +57,73 @@ public class PromotionService {
                 PromotionMapper.MAPPER.listPromotionToPromotionResponse(promotionPage.getContent());
         Page<PromotionResponse> responsePage =
                 new PageImpl<>(promotionShortList, pageable, promotionPage.getTotalElements());
+        log.info("getAllPromotionsBySearch() -> exit");
         return responsePage;
     }
 
     public PromotionResponse getPromotion(Long promotionId) {
+        log.info(String.format("getPromotion() -> start, with promotionId: %s", promotionId));
         if (promotionId != null) {
             Optional<Promotion> byId = promotionRepo.findById(promotionId);
-            Promotion promotion = byId.orElseThrow(EntityExistsException::new);
+            Promotion promotion = byId.orElseThrow(EntityNotFoundException::new);
             PromotionResponse response = PromotionMapper.MAPPER.promotionToPromotionResponse(promotion);
+            log.info(String.format("getPromotion() -> exit, return promotionResponse: %s", response));
             return response;
         }
+        log.info("getPromotion() -> exit, return null");
         return null;
     }
 
     public Map<String, String> getPromotionTypes() {
+        log.info("getPromotionTypes() -> start");
         Map<String, String> promotionValuesLabels = new HashMap<>();
         Arrays.stream(PromotionType.values()).forEach(
                 promotionType -> promotionValuesLabels.put(promotionType.name(), promotionType.label));
+        log.info(String.format("getPromotionTypes() -> exit, return Map: %s", promotionValuesLabels));
         return promotionValuesLabels;
     }
 
     public Page<MainCategoryDTO> getCategoriesForPromo(String search, int page, int pageSize) {
-        return mainCategoryService.getMainCategoriesBySearch(search, page, pageSize);
+        log.info(String.format("getCategoriesForPromo() -> start, with page: %s, pageSize: %s, by search: %s", page, pageSize, search));
+        Page<MainCategoryDTO> mainCategoriesBySearch = mainCategoryService.getMainCategoriesBySearch(search, page, pageSize);
+        log.info("getCategoriesForPromo() -> exit");
+        return mainCategoriesBySearch;
     }
 
     public List<SubcategoryDTO> getSubcategoriesByCategory(Long categoryId) {
+        log.info(String.format("getSubcategoriesByCategory() -> start, with categoryId: %s", categoryId));
         MainCategoryDTO mainCategoryById = mainCategoryService.getMainCategoryById(categoryId);
+        log.info(String.format("getSubcategoriesByCategory() -> exit, return MainCategoryDTO: %s", mainCategoryById));
         return mainCategoryById.getSubcategories();
     }
 
     public Page<SubcategoryDTO> getSubcategories(String search, int page, int pageSize) {
-        return subcategoryService.getSubcategories(page, pageSize, search);
+        log.info(String.format("getSubcategories() -> start, with page: %s, pageSize: %s, by search: %s",
+                page, pageSize, search));
+        Page<SubcategoryDTO> subcategories = subcategoryService.getSubcategories(page, pageSize, search);
+        log.info("getSubcategories() -> exit");
+        return subcategories;
     }
 
     public Map<String, String> getPromotionConditions() {
+        log.info("getPromotionConditions() -> start");
         Map<String, String> promotionConditionValuesLabels = new HashMap<>();
         Arrays.stream(PromotionCondition.values()).forEach(
                 promotionCondition -> promotionConditionValuesLabels
                         .put(promotionCondition.name(), promotionCondition.label));
+        log.info("getPromotionConditions() -> exit, return Map: " + promotionConditionValuesLabels);
         return promotionConditionValuesLabels;
     }
 
     public Page<ProductShortResponse> getProducts(String search, int page, int pageSize) {
-        return productService.getProductsBySearch(page, pageSize, search);
+        log.info(String.format("getProducts() -> start, with page: %s, pageSize: %s", page, pageSize));
+        Page<ProductShortResponse> productsBySearch = productService.getProductsBySearch(page, pageSize, search);
+        log.info("getProducts() -> exit");
+        return productsBySearch;
     }
 
     public PromotionResponse updatePromotion(PromotionRequest promotionRequest) {
+        log.info(String.format("updatePromotion() -> start, with id: %s", promotionRequest.getId()));
         Promotion request = PromotionMapper.MAPPER.promotionRequestToPromotion(promotionRequest);
         Optional<Promotion> byId = promotionRepo.findById(request.getId());
         Promotion promotion = byId.orElseThrow(EntityNotFoundException::new);
@@ -112,10 +135,13 @@ public class PromotionService {
         }
 
         Promotion save = promotionRepo.save(request);
-        return Mappers.getMapper(PromotionMapper.class).promotionToPromotionResponse(save);
+        PromotionResponse promotionResponse = Mappers.getMapper(PromotionMapper.class).promotionToPromotionResponse(save);
+        log.info(String.format("updatePromotion() -> exit, success update promotion, with id: %s", save.getId()));
+        return promotionResponse;
     }
 
     public PromotionResponse createPromotion(PromotionRequest promotionRequest) {
+        log.info(String.format("createPromotion() -> start, promotionRequest: %s", promotionRequest));
         Promotion request = PromotionMapper.MAPPER.promotionRequestToPromotion(promotionRequest);
         request.setDateOfCreate(Instant.now());
         request.setUsedCount(0);
@@ -125,21 +151,26 @@ public class PromotionService {
         }
 
         Promotion save = promotionRepo.save(request);
+        log.info(String.format("createPromotion() -> exit, new promotion id: %s", save.getId()));
         return Mappers.getMapper(PromotionMapper.class).promotionToPromotionResponse(save);
     }
 
     private String savePhotoFile(MultipartFile photoFile) {
+        log.info(String.format("savePhotoFile() -> start, with fileName: %s, fileType: %s",
+                photoFile.getName(), photoFile.getContentType()));
         String filename;
         try {
             filename = fileUtil.saveImage(photoFile);
+            log.info(String.format("savePhotoFile() -> success save file, with fileName: %s", filename));
             return filename;
         } catch (IOException e) {
             log.error("Error on save file image, error message: " + e.getMessage());
-            throw new RuntimeException();
+            throw new RuntimeException("Error save file for Promotion");
         }
     }
 
     public Page<PromotionResponse> getAllActivePromotionsBySearch(int page, int pageSize, String search) {
+        log.info(String.format("getAllActivePromotionsBySearch() -> start, with page: %s, pageSize: %s, by search: %s", page, pageSize, search));
         Pageable pageable = PageRequest.of(page, pageSize);
         SearchCriteria likeName = new SearchCriteria("name", ":", search);
         SearchCriteria isActive = new SearchCriteria("isActive", "!", true);
@@ -151,6 +182,7 @@ public class PromotionService {
                 PromotionMapper.MAPPER.listPromotionToPromotionResponse(promotionPage.getContent());
         Page<PromotionResponse> responsePage =
                 new PageImpl<>(promotionShortList, pageable, promotionPage.getTotalElements());
+        log.info("getAllActivePromotionsBySearch() -> exit");
         return responsePage;
     }
 }
---
+++ src/test/java/com/restaurant/restaurant_admin/service/PromotionServiceTest.java
@@ -0,0 +1,282 @@
+package com.restaurant.restaurant_admin.service;
+
+import com.restaurant.restaurant_admin.entity.Promotion;
+import com.restaurant.restaurant_admin.entity.enums.PromotionCondition;
+import com.restaurant.restaurant_admin.entity.enums.PromotionType;
+import com.restaurant.restaurant_admin.model.*;
+import com.restaurant.restaurant_admin.repository.PromotionRepo;
+import com.restaurant.restaurant_admin.repository.specification.PromotionSpecification;
+import com.restaurant.restaurant_admin.utils.UploadFileUtil;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PromotionServiceTest {
+
+    @Mock
+    private PromotionRepo promotionRepo;
+    @Mock
+    private MainCategoryService mainCategoryService;
+    @Mock
+    private SubcategoryService subcategoryService;
+    @Mock
+    private ProductService productService;
+    @Mock
+    private UploadFileUtil fileUtil;
+    @InjectMocks
+    private PromotionService promotionService;
+
+    private Promotion promotion;
+    private List<Promotion> promotions;
+
+
+    @BeforeEach
+    void setUp() {
+        promotion = new Promotion();
+        promotions = new ArrayList<>();
+
+        promotion.setId(1L);
+        promotion.setDateOfCreate(Instant.now());
+        promotion.setIsActive(Boolean.TRUE);
+        promotion.setName("Promotion");
+        promotion.setPromotionType(PromotionType.FOR_PRODUCT);
+
+        for (int i = 0; i < 10; i++) {
+            Promotion promotion1 = new Promotion();
+            promotion1.setId((long) i);
+            promotion1.setDateOfCreate(Instant.now());
+            promotion1.setIsActive(Boolean.TRUE);
+            promotion1.setName("Promotion_" + i);
+            promotion1.setPromotionType(PromotionType.FOR_PRODUCT);
+            promotions.add(promotion1);
+
+        }
+    }
+
+    @Test
+    void getAllPromotions() {
+        Pageable pageable = PageRequest.ofSize(10);
+        Page<Promotion> promotionPage = new PageImpl<>(promotions, pageable, promotions.size());
+        when(promotionRepo.findAll(any(Pageable.class))).thenReturn(promotionPage);
+        Page<PromotionResponse> allPromotions =
+                promotionService.getAllPromotions(pageable.getPageNumber(), pageable.getPageSize());
+        List<PromotionResponse> content = allPromotions.getContent();
+        assertFalse(content.isEmpty());
+
+        for (int i = 0; i < promotions.size(); i++) {
+            Promotion promotion1 = promotions.get(i);
+            PromotionResponse promotionResponse = content.get(i);
+            assertEquals(promotion1.getId(), promotionResponse.getId());
+            assertEquals(promotion1.getName(), promotionResponse.getName());
+            assertEquals(promotion1.getPromotionType(), promotionResponse.getPromotionType());
+        }
+    }
+
+    @Test
+    void getAllPromotionsBySearch() {
+        Pageable pageable = PageRequest.ofSize(10);
+        Page<Promotion> promotionPage = new PageImpl<>(promotions, pageable, promotions.size());
+        when(promotionRepo.findAll(any(PromotionSpecification.class), any(Pageable.class))).thenReturn(promotionPage);
+        Page<PromotionResponse> allPromotions =
+                promotionService.getAllPromotionsBySearch(pageable.getPageNumber(), pageable.getPageSize(), "");
+        List<PromotionResponse> content = allPromotions.getContent();
+        assertFalse(content.isEmpty());
+
+        for (int i = 0; i < promotions.size(); i++) {
+            Promotion promotion1 = promotions.get(i);
+            PromotionResponse promotionResponse = content.get(i);
+            assertEquals(promotion1.getId(), promotionResponse.getId());
+            assertEquals(promotion1.getName(), promotionResponse.getName());
+            assertEquals(promotion1.getPromotionType(), promotionResponse.getPromotionType());
+        }
+    }
+
+    @Test
+    void getPromotion_ifPromotionIsPresent() {
+        when(promotionRepo.findById(promotion.getId())).thenReturn(Optional.of(promotion));
+        PromotionResponse promotion1 = promotionService.getPromotion(promotion.getId());
+        assertNotNull(promotion1);
+        assertEquals(promotion.getId(), promotion1.getId());
+        assertEquals(promotion.getName(), promotion1.getName());
+        assertEquals(promotion.getPromotionType(), promotion1.getPromotionType());
+    }
+
+    @Test
+    void getPromotion_ifPromotionNotFound() {
+        when(promotionRepo.findById(promotion.getId())).thenReturn(Optional.empty());
+        assertThrows(EntityNotFoundException.class, () ->
+                promotionService.getPromotion(promotion.getId()));
+    }
+
+    @Test
+    void getPromotion_ifParameterIsNull() {
+        PromotionResponse promotion1 = promotionService.getPromotion(null);
+        assertNull(promotion1);
+    }
+
+    @Test
+    void getPromotionTypes() {
+        Map<String, String> promotionValuesLabels = new HashMap<>();
+        Arrays.stream(PromotionType.values()).forEach(
+                promotionType -> promotionValuesLabels.put(promotionType.name(), promotionType.label));
+        Map<String, String> promotionTypes = promotionService.getPromotionTypes();
+        assertEquals(promotionValuesLabels.size(), promotionTypes.size());
+    }
+
+    @Test
+    void getCategoriesForPromo() {
+        Pageable pageable = PageRequest.ofSize(10);
+        Page<MainCategoryDTO> mainCategoryDTOPage = new PageImpl<>(
+                List.of(new MainCategoryDTO(), new MainCategoryDTO(), new MainCategoryDTO(), new MainCategoryDTO()),
+                pageable, 4);
+        when(mainCategoryService.getMainCategoriesBySearch("", pageable.getPageNumber(), pageable.getPageSize()))
+                .thenReturn(mainCategoryDTOPage);
+        Page<MainCategoryDTO> categoriesForPromo =
+                promotionService.getCategoriesForPromo("", pageable.getPageNumber(), pageable.getPageSize());
+        List<MainCategoryDTO> content = categoriesForPromo.getContent();
+        assertFalse(content.isEmpty());
+    }
+
+    @Test
+    void getSubcategoriesByCategory() {
+        List<SubcategoryDTO> subcategoryDTOS =
+                List.of(new SubcategoryDTO(), new SubcategoryDTO(), new SubcategoryDTO(), new SubcategoryDTO());
+        MainCategoryDTO mainCategoryDTO = new MainCategoryDTO();
+        mainCategoryDTO.setId(1L);
+        mainCategoryDTO.setSubcategories(subcategoryDTOS);
+        when(mainCategoryService.getMainCategoryById(mainCategoryDTO.getId())).thenReturn(mainCategoryDTO);
+
+        List<SubcategoryDTO> subcategoriesByCategory = promotionService.getSubcategoriesByCategory(mainCategoryDTO.getId());
+        assertFalse(subcategoriesByCategory.isEmpty());
+        assertEquals(subcategoryDTOS.size(), subcategoriesByCategory.size());
+    }
+
+    @Test
+    void getSubcategories() {
+        Pageable pageable = PageRequest.ofSize(10);
+        List<SubcategoryDTO> subcategoryDTOS =
+                List.of(new SubcategoryDTO(), new SubcategoryDTO(), new SubcategoryDTO(), new SubcategoryDTO());
+        Page<SubcategoryDTO> subcategoryDTOPage = new PageImpl<>(subcategoryDTOS, pageable, subcategoryDTOS.size());
+        when(subcategoryService.getSubcategories(pageable.getPageNumber(), pageable.getPageSize(), ""))
+                .thenReturn(subcategoryDTOPage);
+
+        Page<SubcategoryDTO> subcategories =
+                promotionService.getSubcategories("", pageable.getPageNumber(), pageable.getPageSize());
+        List<SubcategoryDTO> content = subcategories.getContent();
+        assertFalse(content.isEmpty());
+        assertEquals(subcategoryDTOS.size(), content.size());
+    }
+
+    @Test
+    void getPromotionConditions() {
+        Map<String, String> promotionConditionValuesLabels = new HashMap<>();
+        Arrays.stream(PromotionCondition.values()).forEach(
+                promotionCondition -> promotionConditionValuesLabels
+                        .put(promotionCondition.name(), promotionCondition.label));
+        Map<String, String> promotionConditions = promotionService.getPromotionConditions();
+        assertEquals(promotionConditionValuesLabels, promotionConditions);
+        assertEquals(promotionConditionValuesLabels.size(), promotionConditions.size());
+    }
+
+    @Test
+    void getProducts() {
+        Pageable pageable = PageRequest.ofSize(10);
+        List<ProductShortResponse> productShortResponses = List.of(
+                new ProductShortResponse(), new ProductShortResponse(), new ProductShortResponse(), new ProductShortResponse());
+        Page<ProductShortResponse> page = new PageImpl<>(productShortResponses, pageable, productShortResponses.size());
+        when(productService.getProductsBySearch(pageable.getPageNumber(), pageable.getPageSize(), ""))
+                .thenReturn(page);
+        Page<ProductShortResponse> products = promotionService.getProducts("", pageable.getPageNumber(), pageable.getPageSize());
+        List<ProductShortResponse> content = products.getContent();
+        assertFalse(content.isEmpty());
+        assertEquals(productShortResponses.size(), content.size());
+    }
+
+    @Test
+    void updatePromotion() {
+        PromotionRequest promotionRequest = new PromotionRequest();
+        promotionRequest.setId(1L);
+        promotionRequest.setPromotionType(PromotionType.FOR_PRODUCT);
+        promotionRequest.setName("Promotion");
+        promotionRequest.setPhotoFile(new MockMultipartFile("file", "fileContent".getBytes()));
+        promotion.setDateOfCreate(Instant.now());
+        promotion.setUsedCount(12);
+
+        when(promotionRepo.findById(promotion.getId())).thenReturn(Optional.of(promotion));
+        when(promotionRepo.save(any(Promotion.class))).thenReturn(promotion);
+        PromotionResponse promotionResponse = promotionService.updatePromotion(promotionRequest);
+
+        assertEquals(promotion.getId(), promotionResponse.getId());
+        assertEquals(promotion.getDateOfCreate(), promotionResponse.getDateOfCreate());
+        assertEquals(promotion.getPromotionType(), promotionResponse.getPromotionType());
+    }
+
+    @Test
+    void createPromotion() {
+        PromotionRequest promotionRequest = new PromotionRequest();
+        promotionRequest.setId(1L);
+        promotionRequest.setPromotionType(PromotionType.FOR_PRODUCT);
+        promotionRequest.setName("Promotion");
+        promotionRequest.setPhotoFile(new MockMultipartFile("file", "fileContent".getBytes()));
+
+        ArgumentCaptor<Promotion> promotionArgumentCaptor = ArgumentCaptor.forClass(Promotion.class);
+
+        when(promotionRepo.save(any(Promotion.class))).thenReturn(promotion);
+        PromotionResponse promotionResponse = promotionService.createPromotion(promotionRequest);
+        verify(promotionRepo).save(promotionArgumentCaptor.capture());
+        Promotion value = promotionArgumentCaptor.getValue();
+        assertNotNull(value.getDateOfCreate());
+        assertEquals(0, value.getUsedCount());
+    }
+
+    @Test
+    void createPromotion_ifSavePhotoCatchException() throws IOException {
+        PromotionRequest promotionRequest = new PromotionRequest();
+        promotionRequest.setId(1L);
+        promotionRequest.setPromotionType(PromotionType.FOR_PRODUCT);
+        promotionRequest.setName("Promotion");
+        promotionRequest.setPhotoFile(new MockMultipartFile("file", "fileContent".getBytes()));
+
+        when(fileUtil.saveImage(any(MultipartFile.class))).thenThrow(IOException.class);
+        assertThrows(RuntimeException.class, () -> promotionService.createPromotion(promotionRequest));
+    }
+
+    @Test
+    void getAllActivePromotionsBySearch() {
+        Pageable pageable = PageRequest.ofSize(10);
+        Page<Promotion> promotionPage = new PageImpl<>(promotions, pageable, promotions.size());
+        when(promotionRepo.findAll(any(Specification.class), any(Pageable.class))).thenReturn(promotionPage);
+        Page<PromotionResponse> allPromotions =
+                promotionService.getAllActivePromotionsBySearch(pageable.getPageNumber(), pageable.getPageSize(), "");
+        List<PromotionResponse> content = allPromotions.getContent();
+        assertFalse(content.isEmpty());
+
+        for (int i = 0; i < promotions.size(); i++) {
+            Promotion promotion1 = promotions.get(i);
+            PromotionResponse promotionResponse = content.get(i);
+            assertEquals(promotion1.getId(), promotionResponse.getId());
+            assertEquals(promotion1.getName(), promotionResponse.getName());
+            assertEquals(promotion1.getPromotionType(), promotionResponse.getPromotionType());
+        }
+    }
+}
