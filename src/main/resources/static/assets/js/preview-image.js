8d3af377eef1e7f522531295cc4402822cc291f1
Oleksandr Lesyk <sasha.lesik.98@gmail.com>
2024-01-03 23:09:30 +0200

    modify preview-image.js

--- src/main/resources/static/assets/js/preview-image.js
+++ src/main/resources/static/assets/js/preview-image.js
@@ -3,11 +3,24 @@ $(document).ready(function() {
     $("#previewIconFile").on("change", previewImage);
     function previewImage() {
         if (this.files && this.files[0]) {
+            const type = this.files[0].type;
             const reader = new FileReader();
-            reader.onload = function(e) {
-                $("#previewIcon").attr("src", e.target.result);
+            reader.onload = function (e) {
+                var $previewIcon = $("#previewIcon");
+                if (type === "image/jpeg" || type === "image/jpg" || type === "image/png") {
+                    $previewIcon.attr("src", e.target.result);
+                    $previewIcon.removeClass('is-invalid');
+                    $('.error-message').remove();
+
+                } else {
+                    $previewIcon.addClass("is-invalid")
+                        .after($('<p class="error-message text-center text-danger m-0">' +
+                            'Вибраний файл не є зображенням у форматі jpeg, jpg або png.</p>'
+                        ))
+                }
             };
             reader.readAsDataURL(this.files[0]);
         }
     }
+
 });
